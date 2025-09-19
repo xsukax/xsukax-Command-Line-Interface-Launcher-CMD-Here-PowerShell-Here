ca78e483962afd9e946a8d60364cd99da050fa53
xsukax <xsukax@gmail.com>
2025-09-19 21:31:44 +0300

    Delete PoweShell-Here.bat

--- PoweShell-Here.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "%~dp0"
-start powershell.exe  -NoExit -Command "& {$Host.UI.RawUI.WindowTitle='xsukax PowerShell'; $Host.UI.RawUI.BackgroundColor='Black'; $Host.UI.RawUI.ForegroundColor='Green'; Clear-Host}"
